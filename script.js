e988b88737d27bdc223a479ef9d7d701040ebf53
beatbarter <140558587+beatbarter@users.noreply.github.com>
2023-07-25 18:21:27 -0400

    time to test!

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function handleFileUpload(event) {
   if (!file) return;
 
   // Upload file to Google Drive (Replace 'FOLDER_ID' with your actual folder ID)
-  uploadFileToDrive(file, 'FOLDER_ID')
+  uploadFileToDrive(file, '10e9_TNVAz0rm3sSvEcuh4P1KZujGDqlG')
     .then((fileId) => {
       // Add the uploaded file to the playlist
-      const fileUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
+      const fileUrl = `https://drive.google.com/uc?export=download&id=${10e9_TNVAz0rm3sSvEcuh4P1KZujGDqlG}`;
       const listItem = document.createElement('li');
       listItem.setAttribute('data-src', fileUrl);
       listItem.textContent = file.name;
@@ -24,11 +24,11 @@ function handleFileUpload(event) {
 
 function uploadFileToDrive(file, folderId) {
   return new Promise((resolve, reject) => {
-    const accessToken = 'YOUR_GOOGLE_DRIVE_ACCESS_TOKEN';
+    const accessToken = 'ya29.a0AbVbY6OYL0vSn61pinFr62wOrnpGbqo_rSmQUDH3hjA-xCbCKozt-4iJ_aPtrL6PWKPc1BG8iL3T4uhN65ag6fBntkx_HQcnyThXwP7pVwWFVcwPSFsyCy7DBsRBMGNdpz4iIZgmIfOIp_6Tvfm81Q_2U4iaaCgYKAcsSARESFQFWKvPlp6SqxfJnLnbR0_2rdaZYXg0163';
 
     const xhr = new XMLHttpRequest();
-    xhr.open('POST', `https://www.googleapis.com/upload/drive/v3/files?uploadType=media&supportsAllDrives=true&parents=${folderId}`);
-    xhr.setRequestHeader('Authorization', `Bearer ${accessToken}`);
+    xhr.open('POST', `https://www.googleapis.com/upload/drive/v3/files?uploadType=media&supportsAllDrives=true&parents=${10e9_TNVAz0rm3sSvEcuh4P1KZujGDqlG}`);
+    xhr.setRequestHeader('Authorization', `Bearer ${ya29.a0AbVbY6OYL0vSn61pinFr62wOrnpGbqo_rSmQUDH3hjA-xCbCKozt-4iJ_aPtrL6PWKPc1BG8iL3T4uhN65ag6fBntkx_HQcnyThXwP7pVwWFVcwPSFsyCy7DBsRBMGNdpz4iIZgmIfOIp_6Tvfm81Q_2U4iaaCgYKAcsSARESFQFWKvPlp6SqxfJnLnbR0_2rdaZYXg0163}`);
     xhr.setRequestHeader('Content-Type', 'audio/mpeg');
     xhr.onload = () => {
       if (xhr.status === 200) {

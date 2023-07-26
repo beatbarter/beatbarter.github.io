d24eda58f698c309ec34a0dd7c21a729267a605c
beatbarter <140558587+beatbarter@users.noreply.github.com>
2023-07-25 20:21:27 -0400

    ready to test #2!

--- script.js
+++ script.js
@@ -1,46 +1,67 @@
+let driveApiLoaded = false;
 const fileInput = document.getElementById('fileInput');
+const uploadButton = document.getElementById('uploadButton');
 const playlist = document.getElementById('playlist');
 
-fileInput.addEventListener('change', handleFileUpload);
-
-function handleFileUpload(event) {
-  const file = event.target.files[0];
-  if (!file) return;
-
-  // Upload file to Google Drive (Replace 'FOLDER_ID' with your actual folder ID)
-  uploadFileToDrive(file, '10e9_TNVAz0rm3sSvEcuh4P1KZujGDqlG')
-    .then((fileId) => {
-      // Add the uploaded file to the playlist
-      const fileUrl = `https://drive.google.com/uc?export=download&id=${10e9_TNVAz0rm3sSvEcuh4P1KZujGDqlG}`;
-      const listItem = document.createElement('li');
-      listItem.setAttribute('data-src', fileUrl);
-      listItem.textContent = file.name;
-      playlist.appendChild(listItem);
-    })
-    .catch((error) => {
-      console.error('Error uploading the file:', error);
-    });
-}
-
-function uploadFileToDrive(file, folderId) {
-  return new Promise((resolve, reject) => {
-    const accessToken = 'ya29.a0AbVbY6OYL0vSn61pinFr62wOrnpGbqo_rSmQUDH3hjA-xCbCKozt-4iJ_aPtrL6PWKPc1BG8iL3T4uhN65ag6fBntkx_HQcnyThXwP7pVwWFVcwPSFsyCy7DBsRBMGNdpz4iIZgmIfOIp_6Tvfm81Q_2U4iaaCgYKAcsSARESFQFWKvPlp6SqxfJnLnbR0_2rdaZYXg0163';
-
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', `https://www.googleapis.com/upload/drive/v3/files?uploadType=media&supportsAllDrives=true&parents=${10e9_TNVAz0rm3sSvEcuh4P1KZujGDqlG}`);
-    xhr.setRequestHeader('Authorization', `Bearer ${ya29.a0AbVbY6OYL0vSn61pinFr62wOrnpGbqo_rSmQUDH3hjA-xCbCKozt-4iJ_aPtrL6PWKPc1BG8iL3T4uhN65ag6fBntkx_HQcnyThXwP7pVwWFVcwPSFsyCy7DBsRBMGNdpz4iIZgmIfOIp_6Tvfm81Q_2U4iaaCgYKAcsSARESFQFWKvPlp6SqxfJnLnbR0_2rdaZYXg0163}`);
-    xhr.setRequestHeader('Content-Type', 'audio/mpeg');
-    xhr.onload = () => {
-      if (xhr.status === 200) {
-        const response = JSON.parse(xhr.responseText);
-        resolve(response.id);
-      } else {
-        reject(xhr.statusText);
-      }
-    };
-    xhr.onerror = () => {
-      reject(xhr.statusText);
-    };
-    xhr.send(file);
+// Load the Google Drive API client
+function loadDriveApi() {
+  gapi.client.load('drive', 'v3', () => {
+    driveApiLoaded = true;
+    console.log('Google Drive API loaded.');
   });
 }
+
+// Handle file upload when the "Upload" button is clicked
+uploadButton.addEventListener('click', () => {
+  if (driveApiLoaded) {
+    const file = fileInput.files[0];
+    if (!file) return;
+
+    // Upload the file to Google Drive
+    uploadFileToDrive(file)
+      .then((response) => {
+        const fileId = response.result.id;
+        const fileUrl = `https://drive.google.com/uc?export=download&id=10e9_TNVAz0rm3sSvEcuh4P1KZujGDqlG`;
+        
+        // Add the uploaded file to the community playlist
+        const listItem = document.createElement('li');
+        listItem.setAttribute('data-src', fileUrl);
+        listItem.textContent = file.name;
+        playlist.appendChild(listItem);
+        
+        console.log(`File uploaded: ${file.name}`);
+      })
+      .catch((error) => {
+        console.error('Error uploading the file:', error);
+      });
+  }
+});
+
+// Upload file to Google Drive
+function uploadFileToDrive(file) {
+  return gapi.client.drive.files.create({
+    resource: {
+      name: file.name,
+      mimeType: 'audio/mpeg',
+      parents: [ '10e9_TNVAz0rm3sSvEcuh4P1KZujGDqlG' ] // Replace with your folder ID
+    },
+    media: {
+      mimeType: 'audio/mpeg',
+      body: file
+    }
+  });
+}
+
+// Initialize the API client and load the Google Drive API
+function initClient() {
+  gapi.client.init({
+    apiKey: 'ya29.a0AbVbY6NEq8dlu9vAf4a_VXDud0ScLw-6EMQPYEu3JqJ8Nrzjyr8Ur6Wuc9AZIPpjOZD2osLbzphRkd4GUu4C-_wpHMKtxJo3G65tk0gj8zc2L_Pd70Hz6zZooairiMPJDa4Y21kbUYTNLdeyiWYIc-hQ5S2caCgYKAewSARESFQFWKvPlIHthKlXGL4SEE9B5XwuJCQ0163', // Replace with your API key
+    discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
+    scope: 'https://www.googleapis.com/auth/drive.file'
+  }).then(() => {
+    loadDriveApi();
+  });
+}
+
+// Load the API client and authenticate the user
+gapi.load('client', initClient);

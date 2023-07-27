f85d9ed9ec8a0c8f02aa64b53f5819bcdb76e38d
beatbarter <140558587+beatbarter@users.noreply.github.com>
2023-07-27 10:39:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
-if (event.target.files) {
-  const reader = new FileReader();
-  reader.onload = () => {
-    const file = event.target.files[0];
-    const dataURL = reader.result;
-    // Do something with the file data.
-  };
-  reader.readAsDataURL(event.target.files[0]);
-} else {
-  // No file was selected.
+// Function to upload the file to Google Drive
+function uploadFile(file) {
+    const metadata = {
+        name: file.name,
+        mimeType: file.type,
+        parents: ['10e9_TNVAz0rm3sSvEcuh4P1KZujGDqlG'], // Replace with the folder ID where you want to store the files
+    };
+
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        const base64Data = e.target.result.split(',')[1]; // Get the base64 data after the comma
+        const blob = new Blob([base64Data], { type: file.type });
+        const formData = new FormData();
+        formData.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
+        formData.append('file', blob);
+
+        gapi.client.drive.files.create({
+            resource: metadata,
+            media: { body: formData },
+            fields: 'id',
+        }).then(function (response) {
+            console.log('File uploaded successfully:', response.result);
+            displayUploadedFile(file.name, response.result.id);
+        }).catch(function (error) {
+            console.error('Error uploading file:', error);
+        });
+    };
+    reader.readAsDataURL(file);
+}
+
+// Function to display the uploaded files in the shared playlist with audio playback
+function displayUploadedFile(filename, fileId) {
+    const playlist = document.getElementById('playlist');
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = `https://drive.google.com/uc?export=download&id=${fileId}`;
+    a.innerText = filename;
+
+    // Create an audio element and set its source to the Google Drive link
+    const audio = document.createElement('audio');
+    audio.controls = true;
+    audio.src = `https://drive.google.com/uc?export=download&id=${fileId}`;
+
+    li.appendChild(a);
+    li.appendChild(audio);
+    playlist.appendChild(li);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -35,55 +71,9 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    // Function to upload the file to Google Drive
-    function uploadFile(file) {
-        const metadata = {
-            name: file.name,
-            mimeType: file.type,
-            parents: ['10e9_TNVAz0rm3sSvEcuh4P1KZujGDqlG'], // Replace with the folder ID where you want to store the files
-        };
-
-        const reader = new FileReader();
-        reader.onload = function (e) {
-            const base64Data = e.target.result.split(',')[1]; // Get the base64 data after the comma
-            const blob = new Blob([base64Data], { type: file.type });
-            const formData = new FormData();
-            formData.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
-            formData.append('file', blob);
-
-            gapi.client.drive.files.create({
-                resource: metadata,
-                media: { body: formData },
-                fields: 'id',
-            }).then(function (response) {
-                console.log('File uploaded successfully:', response.result);
-                displayUploadedFile(file.name, response.result.id);
-            }).catch(function (error) {
-                console.error('Error uploading file:', error);
-            });
-        };
-        reader.readAsDataURL(file);
-    }
-
-    // Function to display the uploaded files in the shared playlist with audio playback
-    function displayUploadedFile(filename, fileId) {
-        const playlist = document.getElementById('playlist');
-        const li = document.createElement('li');
-        const a = document.createElement('a');
-        a.href = `https://drive.google.com/uc?export=download&id=${fileId}`;
-        a.innerText = filename;
-
-        // Create an audio element and set its source to the Google Drive link
-        const audio = document.createElement('audio');
-        audio.controls = true;
-        audio.src = `https://drive.google.com/uc?export=download&id=${fileId}`;
-
-        li.appendChild(a);
-        li.appendChild(audio);
-        playlist.appendChild(li);
-    }
-
-    document.getElementById('uploadForm').addEventListener('submit', handleFormSubmit);
+    document.getElementById('uploadForm').addEventListener('submit', function (event) {
+        handleFormSubmit(event);
+    });
 
     // Load the Google Drive API client library
     gapi.load('client:auth2', initClient);

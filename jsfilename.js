44e6856fa0601cef20b96847147aa26b0feaeeed
beatbarter <140558587+beatbarter@users.noreply.github.com>
2023-07-27 11:03:47 -0400

    Update jsfilename.js

--- jsfilename.js
+++ jsfilename.js
@@ -1,16 +1,33 @@
-// jsfilename.js
 document.addEventListener('DOMContentLoaded', function () {
-    // Function to initialize Google Drive API with your OAuth2 client ID
+    // Function to initialize Google Sign-In
     function initClient() {
-        gapi.load('client:auth2', () => {
-            gapi.client.init({
-                clientId: '637925006165-ikssgig311emd4t83dbn03867591ka8v.apps.googleusercontent.com', // Replace with your own client ID
-                scope: 'https://www.googleapis.com/auth/drive.file',
-            }).then(function () {
-                console.log('Google Drive API initialized.');
-            }).catch(function (error) {
-                console.error('Error initializing Google Drive API:', error);
-            });
+        google.accounts.id.initialize({
+            client_id: 'YOUR_CLIENT_ID', // Replace with your actual OAuth2 client ID
+            callback: handleSignIn,
+        });
+    }
+
+    // Function to handle user sign-in status
+    function handleSignIn(response) {
+        if (response.status === google.accounts.id.Status.OK) {
+            // User is signed in, you can now initialize the Google Drive API.
+            gapi.load('client', initGoogleDrive);
+        } else {
+            // User is not signed in or has not granted the required permissions.
+            console.error('Google Sign-In failed or permissions not granted.');
+        }
+    }
+
+    // Function to initialize the Google Drive API
+    function initGoogleDrive() {
+        gapi.client.init({
+            apiKey: 'YOUR_API_KEY', // Replace with your actual API key
+            clientId: 'YOUR_CLIENT_ID', // Replace with your actual OAuth2 client ID
+            scope: 'https://www.googleapis.com/auth/drive.file',
+        }).then(function () {
+            console.log('Google Drive API initialized.');
+        }).catch(function (error) {
+            console.error('Error initializing Google Drive API:', error);
         });
     }
 
@@ -26,42 +43,42 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-  // Function to upload the file to Google Drive
-function uploadFile(file) {
-    const metadata = {
-        name: file.name,
-        mimeType: file.type,
-        parents: ['10e9_TNVAz0rm3sSvEcuh4P1KZujGDqlG'], // Replace with the folder ID where you want to store the files
-    };
+    // Function to upload the file to Google Drive
+    function uploadFile(file) {
+        const metadata = {
+            name: file.name,
+            mimeType: file.type,
+            parents: ['10e9_TNVAz0rm3sSvEcuh4P1KZujGDqlG'], // Replace with the folder ID where you want to store the files
+        };
 
-    const reader = new FileReader();
-    reader.onload = function (e) {
-        const base64Data = e.target.result.split(',')[1]; // Get the base64 data after the comma
-        const blob = new Blob([base64Data], { type: file.type });
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            const base64Data = e.target.result.split(',')[1]; // Get the base64 data after the comma
+            const blob = new Blob([base64Data], { type: file.type });
 
-        // Use the new gapi.client.request method to upload the file
-        gapi.client.request({
-            path: '/drive/v3/files',
-            method: 'POST',
-            params: {
-                uploadType: 'multipart',
-            },
-            headers: {
-                'Content-Type': 'multipart/related;',
-            },
-            body: {
-                metadata: JSON.stringify(metadata),
-                file: blob,
-            },
-        }).then(function (response) {
-            console.log('File uploaded successfully:', response.result.id);
-            displayUploadedFile(file.name, response.result.id);
-        }).catch(function (error) {
-            console.error('Error uploading file:', error);
-        });
-    };
-    reader.readAsDataURL(file);
-}
+            // Use the new gapi.client.request method to upload the file
+            gapi.client.request({
+                path: '/drive/v3/files',
+                method: 'POST',
+                params: {
+                    uploadType: 'multipart',
+                },
+                headers: {
+                    'Content-Type': 'multipart/related;',
+                },
+                body: {
+                    metadata: JSON.stringify(metadata),
+                    file: blob,
+                },
+            }).then(function (response) {
+                console.log('File uploaded successfully:', response.result.id);
+                displayUploadedFile(file.name, response.result.id);
+            }).catch(function (error) {
+                console.error('Error uploading file:', error);
+            });
+        };
+        reader.readAsDataURL(file);
+    }
 
     // Function to display the uploaded files in the shared playlist with audio playback
     function displayUploadedFile(filename, fileId) {
@@ -83,6 +100,9 @@ function uploadFile(file) {
 
     document.getElementById('uploadForm').addEventListener('submit', (event) => handleFormSubmit(event));
 
-    // Load the Google Drive API client library
-    gapi.load('client:auth2', initClient);
+    // Load the Google Sign-In library and initialize it
+    google.accounts.id.initialize({
+        client_id: 'YOUR_CLIENT_ID', // Replace with your actual OAuth2 client ID
+        callback: initClient,
+    });
 });
